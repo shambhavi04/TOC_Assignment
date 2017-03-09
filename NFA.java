47c5474d3696baabb791bc4091bbbe0365815822
TunishaSingal <tu306@snu.edu.in>
2017-03-09 22:37:02 +0530

    Add files via upload

---
+++ NFA.java
@@ -0,0 +1,39 @@
+mport java.util.LinkedList;
+
+
+
+
+public class NFA {
+
+	private LinkedList<State> nfa;
+
+
+
+	public NFA () {
+
+		this.setNfa(new LinkedList<State> ());
+
+		this.getNfa().clear();
+
+	}
+
+
+
+
+	public LinkedList<State> getNfa() {
+
+		return nfa;
+
+	}
+
+
+
+
+	public void setNfa(LinkedList<State> nfa) {
+
+		this.nfa = nfa;
+
+	}
+
+}
+
