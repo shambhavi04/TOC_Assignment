651ec7b46c8cff78e53bf88e477be69a8449621d
TunishaSingal <tu306@snu.edu.in>
2017-03-25 22:04:03 +0530

    Update NFA.java

--- NFA.java
+++ NFA.java
@@ -1,39 +1,14 @@
-mport java.util.LinkedList;
-
-
-
-
+import java.util.LinkedList;
 public class NFA {
-
 	private LinkedList<State> nfa;
-
-
-
 	public NFA () {
-
 		this.setNfa(new LinkedList<State> ());
-
 		this.getNfa().clear();
-
 	}
-
-
-
-
 	public LinkedList<State> getNfa() {
-
 		return nfa;
-
 	}
-
-
-
-
 	public void setNfa(LinkedList<State> nfa) {
-
 		this.nfa = nfa;
-
 	}
-
 }
-
