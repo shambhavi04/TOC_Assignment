bea300c4fe9a117d718148c54f6d3ecb0d3a4e3c
barkha29 <barkha.agarwal29@gmail.com>
2017-03-24 22:16:00 -0700

    Update Question_1.c

--- Question_1.c
+++ Question_1.c
@@ -1,55 +1,78 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+package toc;
 
-//function to get the next state of transition table
-int new_state(char *pattern, int len1, int state, int j)
-{	if (state < len1 && j == pattern[state])	//checking if pattern matches with string
-  	return state+1;					//increment the stat
- 	
-  	int next_state, i; 		// next_state stores the result
+/**
+ *
+ * @author Barkha Agrawal
+ */
+import java.util.Scanner;
  
-  	for (next_state = state; next_state > 0; next_state--)
-  	{	if(pattern[next_state-1] == j)
-  		{	for(i = 0; i < next_state-1; i++)
-  			{	if (pattern[i] != pattern[state-next_state+1+i])
-  					break;
-  			}
-  			if (i == next_state-1)
-  				return next_state;
-  		}
-  	}
- 	return 0;
-}
+public class TOC
+{
+    public static final int NCHARS = 256;
+   
+    
+    //prints all occurrences of pattern in string 
+    public static void pattern(char[] pat, char[] string)
+    {
+        int len1 = pat.length;  //length of pattern
+        int len2 = string.length;   //length of string
+        int[][] table = new int[len1 + 1][NCHARS];
+        construct(pat, len1, table);  //construction of transition table
+      
+        int i, state = 0;
+        for (i = 0; i < len2; i++)
+        {
+            state = table[state][string[i]];
+            if (state == len1)
+            {
+                System.out.print(pat);
+                System.out.println(" String Found at Position " + (i - len1 + 1));
+            }
+        }
+    }
+    
+    //construct transition table using the algorithm
+     public static void construct(char[] pat, int len1, int[][] table)
+    {
+        int state, x;
+        for (state = 0; state <= len1; ++state)
+            for (x = 0; x < NCHARS; ++x)
+                table[state][x] = new_state(pat, len1, state, x);
+    }
+
+    
+    //function to get the next state of transition table
+     public static int new_state(char[] pat, int len1, int state, int x)
+    {
+        
+        if (state < len1 && x == pat[state])    //checking if pattern matches with string
+            return state + 1;                   //increment the state
+        int next_state, i;                 //next_state stores the result
+       
+        for (next_state = state; next_state > 0; next_state--)
+        {
+            if (pat[next_state - 1] == x)
+            {
+                for (i = 0; i < next_state - 1; i++)
+                {
+                    if (pat[i] != pat[state - next_state + 1 + i])
+                        break;
+                }
+                if (i == next_state - 1)
+                    return next_state;  
+            }
+        }
+        return 0;
+    }
  
-//construct transition table using the algorithm
-void construct(char *pattern, int len1, int table[][500])
-{	int state, j;
-  	for (state = 0; state <= len1; state++)
-  		for (j = 0; j < 500; j++)
-  			table[state][j] = new_state(pattern, len1, state, j);
-}
- 
-//prints all occurrences of pattern in txt 
-void search(char *pattern, char *string)
-{	int len1 = strlen(pattern);
-  	int len2 = strlen(string);
- 	int table[len1+1][500];
- 	construct(pattern, len1, table); 	//construction of transition table
- 
-	int i, state=0;
-  	for (i = 0; i < len2; i++)
-  	{	state = table[state][string[i]];
-  		if (state == len1)
-			printf ("\nPattern found at position %d", i-len1+1);
-  	}
-  	printf("\n");
-}
- 
-int main()
-{	//take pattern as an input	
-	char *string = (char *) "an apple an day an";   //entering the string
-  	char *pattern = (char *) "an";            //entering the pattern
-  	search(pattern, string);		   //calling search function
-	return 0;
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String main = input.nextLine();
+        System.out.println("Enter the pattern you are searching for: ");
+        String pattern = input.nextLine();
+        pattern(pattern.toCharArray(), main.toCharArray());
+        input.close();
+    }
 }
